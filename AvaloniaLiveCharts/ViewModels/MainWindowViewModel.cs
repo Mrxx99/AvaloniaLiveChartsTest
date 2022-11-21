d4e248058eec884ebff6196b190dd1db7f5c7372
mrxx99 <mrxx99@googlemail.com>
2022-11-21 22:16:23 +0100

    Add project files.

---
+++ AvaloniaLiveCharts/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Avalonia;
+
+namespace AvaloniaLiveCharts.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+
+    public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+            };
+}
---
+++ AvaloniaLiveCharts/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaLiveCharts.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}
